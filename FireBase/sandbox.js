2f4a11ece5544d0a52a2f5c64483cbeb9e582cb0
Soumodip Das <soumodipd567@gmail.com>
2023-01-11 15:13:44 +0530

    cmt done

---
+++ FireBase/sandbox.js
@@ -0,0 +1,61 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD4FjfNloTRrk-OCIr2gi5wyEsKA4SiT3U",
+  authDomain: "fir-contact-4506c.firebaseapp.com",
+  databaseURL: "https://fir-contact-4506c-default-rtdb.firebaseio.com",
+  projectId: "fir-contact-4506c",
+  storageBucket: "fir-contact-4506c.appspot.com",
+  messagingSenderId: "578586322814",
+  appId: "1:578586322814:web:de6ef60811eed7cbda352f",
+  measurementId: "G-LBG07RNQZ4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
